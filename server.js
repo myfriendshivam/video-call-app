b35d8769433db5f074def76a37c5f5683692afc4
myfriendshivam <shivamkant333@gmail.com>
2025-10-02 13:14:38 +0530

    update package.json

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
 );
